c51bec72ae0af460600cc3ccf741d2b415ff2fc9
Piotr Karol <teropod2601@gmail.com>
2024-04-28 20:15:11 +0200

    Added end-of-game detection

--- src/main/java/com/practice/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/practice/tictactoe/TicTacToeApplication.java
@@ -7,11 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TicTacToeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tictactoe-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeApplication.class.getResource("tictactoe-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Tic-tac-toe");
         stage.setScene(scene);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.practice.tictactoe {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.practice.tictactoe to javafx.fxml;
